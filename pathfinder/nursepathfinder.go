1c420d43c942d0c072f2f089185fe36825eac05f
okeith12 <35121941+okeith12@users.noreply.github.com>
2026-01-25 13:04:40 -0600

    Fix nurse pathfinder

--- pathfinder/nursepathfinder.go
+++ pathfinder/nursepathfinder.go
@@ -37,80 +37,59 @@ func (np *NursePathfinder) MoveTowardLarvae(world *types.World, colony *types.Co
 	return np.MoveTowardTarget(world, colony, nurse, larvaePos, true)
 }
 
-// MoveTowardTarget moves nurse toward a target, going around queen if needed
+// MoveTowardTarget moves nurse toward a target, can pass through queen's cell
 func (np *NursePathfinder) MoveTowardTarget(world *types.World, colony *types.Colony, nurse *types.NurseAnt, target types.Position, goAroundQueen bool) bool {
+	curX := nurse.Position.X
+	curY := nurse.Position.Y
+
 	// All 8 directions
 	directions := [][2]int{
-		{0, -1}, {0, 1}, {-1, 0}, {1, 0},
+		{-1, 0}, {1, 0}, {0, -1}, {0, 1},
 		{-1, -1}, {1, -1}, {-1, 1}, {1, 1},
 	}
 
-	// Find the best direction that gets us closer to target
-	bestDir := -1
-	bestDist := ManhattanDistance(nurse.Position, target)
-
-	for i, dir := range directions {
-		newX := nurse.Position.X + dir[0]
-		newY := nurse.Position.Y + dir[1]
-
-		// Skip queen's position
-		if newX == colony.QueenPosition.X && newY == colony.QueenPosition.Y {
-			continue
-		}
-
-		// Check if we can move or dig there
-		if !CanMoveTo(world, newX, newY) && !CanDigTo(world, newX, newY) {
-			continue
-		}
-
-		// Calculate new distance to target
-		newDist := util.Abs(newX-target.X) + util.Abs(newY-target.Y)
-
-		// Only accept if it gets us closer
-		if newDist < bestDist {
-			bestDist = newDist
-			bestDir = i
-		}
-	}
-
-	// If we found a direction that gets us closer, take it
-	if bestDir >= 0 {
-		newX := nurse.Position.X + directions[bestDir][0]
-		newY := nurse.Position.Y + directions[bestDir][1]
-
-		if CanMoveTo(world, newX, newY) {
-			MoveAnt(world, nurse, newX, newY)
-			return true
-		}
-		if CanDigTo(world, newX, newY) {
-			DigAndMove(world, nurse, newX, newY)
-			return true
-		}
-	}
-
-	// If no direction gets us closer, we need to go around
-	// Try any direction that doesn't go backwards
 	currentDist := ManhattanDistance(nurse.Position, target)
 
+	// Find direction that gets us closer
 	for _, dir := range directions {
-		newX := nurse.Position.X + dir[0]
-		newY := nurse.Position.Y + dir[1]
+		newX := curX + dir[0]
+		newY := curY + dir[1]
 
-		// Skip queen's position
-		if newX == colony.QueenPosition.X && newY == colony.QueenPosition.Y {
+		newDist := util.Abs(newX-target.X) + util.Abs(newY-target.Y)
+
+		// Only move if it gets us closer
+		if newDist >= currentDist {
 			continue
 		}
 
-		// Accept same distance (going around)
-		newDist := util.Abs(newX-target.X) + util.Abs(newY-target.Y)
-		if newDist > currentDist+1 {
-			continue // Don't go too far backwards
+		// If it's the queen's cell, swap positions with queen
+		if newX == colony.QueenPosition.X && newY == colony.QueenPosition.Y {
+			// Swap: nurse goes to queen's spot, queen goes to nurse's spot
+			queenCell := world.GetCell(colony.QueenPosition.X, colony.QueenPosition.Y)
+			nurseCell := world.GetCell(curX, curY)
+
+			// Move queen to nurse's old position
+			colony.Queen.Position.X = curX
+			colony.Queen.Position.Y = curY
+			nurseCell.Occupant = colony.Queen
+
+			// Update colony's queen position so everyant knows
+			colony.QueenPosition.X = curX
+			colony.QueenPosition.Y = curY
+
+			// Move nurse to queen's position
+			nurse.Position.X = newX
+			nurse.Position.Y = newY
+			queenCell.Occupant = nurse
+
+			return true
 		}
 
 		if CanMoveTo(world, newX, newY) {
 			MoveAnt(world, nurse, newX, newY)
 			return true
 		}
+
 		if CanDigTo(world, newX, newY) {
 			DigAndMove(world, nurse, newX, newY)
 			return true
